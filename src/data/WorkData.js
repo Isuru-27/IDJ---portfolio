c0ad10d9a01099935443bcf0ebed9f3affa1ae6f
Isuru D Rajamanthri <dilshanisuru2000@gmail.com>
2024-10-18 12:01:45 +0530

    IDR portfolio

---
+++ src/data/WorkData.js
@@ -0,0 +1,72 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Enfry clinic Test Report",
+        description:"I created a test report manually, documenting test cases, results, and identified issues to ensure the software's quality and performance.",
+        tags:["Software Testing","Manual Testing"],
+        
+        demo:"https://private-user-images.githubusercontent.com/139687227/358870365-c8157dde-663f-42de-87ad-6049757d8ea2.mp4?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTUiLCJleHAiOjE3Mjg4MjA1MzAsIm5iZiI6MTcyODgyMDIzMCwicGF0aCI6Ii8xMzk2ODcyMjcvMzU4ODcwMzY1LWM4MTU3ZGRlLTY2M2YtNDJkZS04N2FkLTYwNDk3NTdkOGVhMi5tcDQ_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBVkNPRFlMU0E1M1BRSzRaQSUyRjIwMjQxMDEzJTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDI0MTAxM1QxMTUwMzBaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1iNTAzZDE1MDZkZWU3ZTYzYWM1OGJkMmI3YmY0ODI4OGM4YzUxMWI5N2U2YTc2YzZiZTNjN2VhZGY0YjYwYTc2JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCJ9.oiu4uZcmB8hv2Q3bA5IAYEdFFM_SsKuXezGlWC4VntM",
+        github:"https://github.com/Isuru-27/Enfry-Clinic-S-W-Testing?tab=readme-ov-file#enfry-clinic-s-w-testing"
+    },
+    {
+        id:2,
+        name:"Musicians Website",
+        description:"I developed a website for a musician, showcasing their portfolio with photos, music videos, and recordings to highlight their talent and engage their audience.",
+        tags:["HTML","CSS","JavaScript"],
+        
+        demo:"https://pasindu-998.github.io/My-Website/",
+        github:"https://github.com/Isuru-27/Musician"
+        
+    },
+    {
+        id:3,
+        name:"Realtime Face Recognition",
+        description:"I developed real-time face recognition software using image processing techniques to accurately identify individuals through live video feeds.",
+        tags:["python","cv2","Image processing","face recognition"],
+        
+        demo:"https://github.com/Isuru-27/Realtime_Face_Recognition",
+        github:"https://github.com/Isuru-27/Realtime_Face_Recognition"
+    },
+    {
+        id:4,
+        name:"QR Generator",
+        description:"I created a QR code generator that allows users to easily generate and customize QR codes for various applications.",
+        tags:["react","QR Generator"],
+        
+        demo:"https://github.com/Isuru-27/QR-Generator",
+        github:"https://github.com/Isuru-27/QR-Generator"
+       
+    },{
+        id:5,
+        name:"Weather App",
+        description:"I developed a weather app that provides real-time weather updates and forecasts for any location.",
+        tags:["react","API"],
+        
+        demo:"https://github.com/Isuru-27/WeatherApp",
+        github:"https://github.com/Isuru-27/WeatherApp"
+        
+    },
+    {
+        id:6,
+        name:"Sunset Apartment",
+        description:"I created a web application for managing apartments, streamlining tasks such as tenant management, maintenance requests, and lease tracking.",
+        tags:["HTML","CSS","JavaScript","PHP","MySQL"],
+        
+        demo:"https://github.com/Isuru-27/Sunset_Apartment",
+        github:"https://github.com/Isuru-27/Sunset_Apartment"
+        
+    },
+    {
+        id:7,
+        name:"King's food Online Ordering System",
+        description:"I developed an online food ordering platform that allows users to browse menus, place orders, and track deliveries with ease.",
+        tags:["HTML","CSS","JavaScript","PHP","MySQL"],
+        
+        demo:"https://github.com/Isuru-27/King-s-Food-Online-Restaurent",
+        github:"https://github.com/Isuru-27/King-s-Food-Online-Restaurent"
+        
+    },
+  
+] 
